22e40cba242438d9f7b3481021f57bc845226d6f
BeUniqueCpp <be.uniquecpp@gmail.com>
2024-02-15 21:40:05 +0600

    add title and description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,6 @@ import { HomePage } from "@/screens/HomePage/HomePage";
 import { Metadata } from 'next'
  
 
-export const metadata: Metadata = {
-  title: 'smm-consultinggroup',
-}
-
 export default function Home() {
   return <HomePage />
 }
